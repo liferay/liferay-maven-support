75373300005892b773c98f06b383e0ac3f6acbb6
Brian Chan <brian.chan@liferay.com>
2014-10-31 09:06:34 -0700

    MAVEN-123 SF

--- plugins/liferay-maven-plugin/src/main/java/com/liferay/maven/plugins/AbstractLiferayMojo.java
+++ plugins/liferay-maven-plugin/src/main/java/com/liferay/maven/plugins/AbstractLiferayMojo.java
@@ -40,6 +40,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.model.Dependency;
@@ -256,32 +257,44 @@ public abstract class AbstractLiferayMojo extends AbstractMojo {
 
 	protected List<String> getProjectClassPath() throws Exception {
 		List<String> projectClassPath = new ArrayList<String>();
-		List<MavenProject> classPathProjects = new ArrayList<MavenProject>();
 
 		projectClassPath.addAll(getToolsClassPath());
 
-		classPathProjects.add(project);
+		List<MavenProject> classPathMavenProjects = new ArrayList<MavenProject>();
+
+		classPathMavenProjects.add(project);
 
 		for (Object object : project.getDependencyArtifacts()) {
-			Artifact artifact = (Artifact) object;
+			Artifact artifact = (Artifact)object;
 
-			if (artifact.getArtifactHandler().isAddedToClasspath()) {
-				MavenProject dependencyProject = resolveProject(artifact);
+			ArtifactHandler artifactHandler = artifact.getArtifactHandler();
 
-				if ((dependencyProject != null) &&
-					dependencyProject.getCompileSourceRoots().size() > 0) {
-
-					getLog().debug(
-						"Adding project to classpath " + dependencyProject);
-
-					classPathProjects.add(dependencyProject);
-				}
+			if (!artifactHandler.isAddedToClasspath()) {
+				continue;
 			}
+
+			MavenProject dependencyMavenProject = resolveProject(artifact);
+
+			if (dependencyMavenProject == null) {
+				continue;
+			}
+
+			List<String> compileSourceRoots =
+				dependencyMavenProject.getCompileSourceRoots();
+
+			if (compileSourceRoots.isEmpty()) {
+				continue;
+			}
+
+			getLog().debug(
+				"Adding project to classpath " + dependencyMavenProject);
+
+			classPathMavenProjects.add(dependencyMavenProject);
 		}
 
-		for (MavenProject classPathProject : classPathProjects) {
+		for (MavenProject classPathMavenProject : classPathMavenProjects) {
 			for (Object object :
-					classPathProject.getCompileClasspathElements()) {
+					classPathMavenProject.getCompileClasspathElements()) {
 
 				String path = (String)object;
 
