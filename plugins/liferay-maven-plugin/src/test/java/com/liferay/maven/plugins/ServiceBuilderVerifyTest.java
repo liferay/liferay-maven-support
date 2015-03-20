a08a2e6c1def19d634602bb84b1b58a2ea123cd7
Gregory Amerson <gregory.amerson@liferay.com>
2015-03-20 09:59:34 +0800

    MAVEN-143 sort

--- plugins/liferay-maven-plugin/src/test/java/com/liferay/maven/plugins/ServiceBuilderVerifyTest.java
+++ plugins/liferay-maven-plugin/src/test/java/com/liferay/maven/plugins/ServiceBuilderVerifyTest.java
@@ -29,39 +29,6 @@ import org.apache.maven.it.util.ResourceExtractor;
  */
 public class ServiceBuilderVerifyTest extends TestCase {
 
-	public void testBuildServiceResolveProject() throws Exception {
-		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
-			"/projects/servicebuilder/testProject");
-
-		assertTrue(testDir.exists());
-
-		Verifier verifier = new Verifier(testDir.getAbsolutePath());
-
-		verifier.deleteArtifact("it", "testProject", "1.0", "pom");
-		verifier.deleteArtifact("it", "testProject-portlet", "1.0", "war");
-		verifier.deleteArtifact("it",
-			"testProject-portlet-service", "1.0", "jar");
-
-		verifier.setMavenDebug(true);
-
-		List<String> cliOptions = new ArrayList<String>();
-
-		cliOptions.add("-P");
-		cliOptions.add("6.2.2");
-
-		cliOptions.add("-pl");
-		cliOptions.add("testProject-portlet");
-
-		verifier.setCliOptions(cliOptions);
-
-		verifier.executeGoal("liferay:build-service");
-
-		verifier.verifyTextInLog("Resolved dependency project MavenProject: "
-			+ "it:testProject-portlet-service");
-
-		verifier.resetStreams();
-	}
-
 	private void buildServiceGeneratedClass(String profileId)
 		throws Exception {
 
@@ -106,5 +73,38 @@ public class ServiceBuilderVerifyTest extends TestCase {
 
 	public void testBuildServiceGeneratedClass62() throws Exception {
 		buildServiceGeneratedClass("6.2.2");
+	}
+
+	public void testBuildServiceResolveProject() throws Exception {
+		File testDir = ResourceExtractor.simpleExtractResources(getClass(),
+			"/projects/servicebuilder/testProject");
+
+		assertTrue(testDir.exists());
+
+		Verifier verifier = new Verifier(testDir.getAbsolutePath());
+
+		verifier.deleteArtifact("it", "testProject", "1.0", "pom");
+		verifier.deleteArtifact("it", "testProject-portlet", "1.0", "war");
+		verifier.deleteArtifact("it",
+			"testProject-portlet-service", "1.0", "jar");
+
+		verifier.setMavenDebug(true);
+
+		List<String> cliOptions = new ArrayList<String>();
+
+		cliOptions.add("-P");
+		cliOptions.add("6.2.2");
+
+		cliOptions.add("-pl");
+		cliOptions.add("testProject-portlet");
+
+		verifier.setCliOptions(cliOptions);
+
+		verifier.executeGoal("liferay:build-service");
+
+		verifier.verifyTextInLog("Resolved dependency project MavenProject: "
+			+ "it:testProject-portlet-service");
+
+		verifier.resetStreams();
 	}
 }
